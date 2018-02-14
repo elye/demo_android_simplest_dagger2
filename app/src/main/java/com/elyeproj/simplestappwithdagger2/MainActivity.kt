b12cea6db4be1b112510ac26d87c79d623937599
elye <elye.project@gmail.com>
2018-02-14 19:52:40 +1100

    Change builder().build() to create(). More concise.

--- app/src/main/java/com/elyeproj/simplestappwithdagger2/MainActivity.kt
+++ app/src/main/java/com/elyeproj/simplestappwithdagger2/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        DaggerMagicBox.builder().build().poke(this)
+        DaggerMagicBox.create().poke(this)
         text_view.text = info.text
     }
 }
